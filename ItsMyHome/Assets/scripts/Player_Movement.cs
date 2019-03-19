9d417d1fdd9f0eed4a4f633c895b8aa94dc0ee26
NeoTech32 <39991767+NeoTech32@users.noreply.github.com>
2019-03-19 15:07:37 +0100

    mouvement + camera rotative

--- ItsMyHome/Assets/scripts/Player_Movement.cs
+++ ItsMyHome/Assets/scripts/Player_Movement.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody theRB;
+    public Vector3 moveDirection;
+    public CharacterController controller;
+    
     // Start is called before the first frame update
     void Start()
     {
-            
+      theRB = GetComponent<Rigidbody>();
+      controller = GetComponent<CharacterController>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f , Input.GetAxis("Vertical") * moveSpeed);
+
+
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        controller.Move(moveDirection);
+
+
     }
 }
